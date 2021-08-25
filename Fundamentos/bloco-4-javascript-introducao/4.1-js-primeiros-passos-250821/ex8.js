3a49aa39f010056642bfe13ad8a29bfd73dcbdb1
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-25 18:49:59 -0300

    C7 Add EX8 B4d1

---
+++ Fundamentos/bloco-4-javascript-introducao/4.1-js-primeiros-passos-250821/ex8.js
@@ -0,0 +1,14 @@
+let numberA;
+let numberB;
+let numberC;
+
+numberA = 1;
+numberB = 7;
+numberC = 5;
+
+if (numberA % 2 == 0 || numberB % 2 == 0 || numberC % 2 == 0) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
