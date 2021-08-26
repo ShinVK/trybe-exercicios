0176b67012912dd6472ec1afda71364fd75a7c2d
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-26 18:50:02 -0300

    C8 add other way to achive objective's exbonus3

--- Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/bonus1.js
+++ Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/bonus1.js
@@ -1,15 +1,16 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let newnumbers = [];
-let newNumber = 0;
+
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    
-    newNumber = numbers[secondIndex] * numbers[index];
-    
-    
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
   }
-  newnumbers.push(newNumber);
 }
-console.log(newnumbers);
+
+console.log(numbers)
+
