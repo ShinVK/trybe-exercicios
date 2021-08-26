aadacf198d5c5d2a3d059e763bb0c2436d1a0411
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-26 18:41:30 -0300

    C7 add ex bonus 2

--- Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/bonus1.js
+++ Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/bonus1.js
@@ -1,14 +1,15 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newnumbers = [];
+let newNumber = 0;
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] > numbers[secondIndex]) {
-      let position = numbers[index];
-      numbers[index] = numbers[secondIndex];
-      numbers[secondIndex] = position;
-    }
+    
+    newNumber = numbers[secondIndex] * numbers[index];
+    
+    
   }
-  
+  newnumbers.push(newNumber);
 }
-console.log(numbers);
+console.log(newnumbers);
