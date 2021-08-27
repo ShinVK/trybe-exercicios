fdbbb0b5335616f711e7df436612d96532b645b1
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-27 15:04:39 -0300

    Change some algorithms from lecture 4.2

--- Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/ex5.js
+++ Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/ex5.js
@@ -1,4 +1,4 @@
-let numbers = [10000, 5, 9, 3, 19, 70, 8, 100, 2, 500, 1000, 35, 27];
+let numbers = [10, 5, 9, 3, 19, 70, 8, 100, 2, 500, 1000, 35, 27];
 let numAtual = 0;
 let maiorNum = 0;
 let maior = 0;
@@ -9,7 +9,7 @@ for (let index = 0; index < numbers.length; index +=1) {
   if (numAtual > maior) {
     maior = numAtual;
    
-  } else if (maior > numAtual) {
+  } else {
     maiorNum = maior
     
   }
--- Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/ex6.js
+++ Fundamentos/bloco-4-javascript-introducao/4.2/exercicios/ex6.js
@@ -1,15 +1,11 @@
 let numbers = [4, 9, 2, 18, 70, 8, 100, 2, 36, 28];
 let contadorImpar = 0;
-let maiorNum = 0;
-let maior = 0;
+
 
 for (let index = 0; index < numbers.length; index +=1) {
   numAtual = numbers[index]
 
-  if (numAtual % 2 == 0) {
-    
-   
-  } else {
+  if (numAtual % 2 !== 0) {
     
     contadorImpar +=1;
     
