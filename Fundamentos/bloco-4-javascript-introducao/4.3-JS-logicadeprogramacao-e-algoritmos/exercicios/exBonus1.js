7ddbf2ac45ed18c2d1ea4211cc131fd93eb748fe
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-27 23:47:42 -0300

    C5 add ex bonus 1 B4d3

---
+++ Fundamentos/bloco-4-javascript-introducao/4.3-JS-logicadeprogramacao-e-algoritmos/exercicios/exBonus1.js
@@ -0,0 +1,50 @@
+const n = 19; //tem que ser ímpar
+let line = '';
+
+const star = "*"
+const _blank = " ";
+// let positionStarRight = 1 + blank_space;
+// let positionStarLeft = n - blank_space; 
+
+
+
+//se ímpar
+for (let index = 1; index <= n; index += 2) {  //3
+  let line = '';
+  let blank_space = (n - index)/2; //7-3/4 =2
+  let positionStarRight = 1 + blank_space;
+  let positionStarLeft = n - blank_space; 
+
+
+  for (let index2 = 1; index2 <= n; index2 += 1) {  //1
+
+
+    
+    if (index2 <= blank_space && index < n) {  //se 1 menor ou igual a 2 - espaçoembranco? , para o 3 ta certo
+      line += _blank;
+      
+    }
+    else if(index2 <= n - blank_space && index <=1) {  //3 <= 4 
+      line += star;
+    }
+    else if(index2 == positionStarLeft && index < n) {
+      line += star;
+    }
+
+    else if(index2 == positionStarRight && index < n){
+      line += star;
+    }
+
+    else if(index2 >= blank_space && index < n) {
+      line += _blank;
+    }
+    else if(index = n) {
+      line += star;
+    }
+
+    
+  }
+  
+   console.log(line);
+  
+}
