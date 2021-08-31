bd842d4b981be9699b301b9bf903faf5076b5c73
Victor Shin <victor.kamiguchi@gmail.com>
2021-08-31 13:55:47 -0300

    C9 Add a part of ex bonus B4.4

---
+++ Fundamentos/bloco-4-javascript-introducao/4.4-JS-Objetos-e-funcoes/bonus/1.js
@@ -0,0 +1,23 @@
+let romNum = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+}
+
+let romStr = 'XVII'
+
+
+for(let r = 0; r < romStr.length; r += 1) {
+  
+}
+
+
+// for (let i in romNum) {
+//   console.log(romNum[i]);
+  
+// }
+
