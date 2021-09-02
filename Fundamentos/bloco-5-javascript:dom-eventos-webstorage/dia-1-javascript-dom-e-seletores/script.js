b5f04d456f2948b03bcefae24532cd41c932c1b6
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-02 15:19:47 -0300

    C0 add examples from lecture5.1

---
+++ Fundamentos/bloco-5-javascript:dom-eventos-webstorage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,10 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Clube da luta"
+document.getElementById("second-paragraph").innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum neque inventore, itaque quo harum consequatur ipsam? Sunt porro quod nihil illo esse minus, eveniet, aspernatur, voluptatibus reprehenderit magnam alias eius?"
+document.getElementById("subtitle").innerText = "lorem ipsum"
+
+const altpar = document.getElementsByClassName("pEx");
+altpar[0].style.color = "blue";
+
+document.getElementsByTagName("h4")[0].style.color = "green"
