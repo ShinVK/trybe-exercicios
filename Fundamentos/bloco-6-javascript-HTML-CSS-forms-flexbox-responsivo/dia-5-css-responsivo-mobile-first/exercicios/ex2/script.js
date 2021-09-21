7d840cbf5bfd0be97d7870a3860ac241f17ca7e5
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-21 19:12:11 -0300

    C3 Medium resolution - 720px css

--- Fundamentos/bloco-6-javascript-HTML-CSS-forms-flexbox-responsivo/dia-5-css-responsivo-mobile-first/exercicios/ex2/script.js
+++ Fundamentos/bloco-6-javascript-HTML-CSS-forms-flexbox-responsivo/dia-5-css-responsivo-mobile-first/exercicios/ex2/script.js
@@ -2,6 +2,7 @@ const btnChapter = document.querySelector("#btn_chapters");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const mdlChpt = document.querySelector(".chapters__content");
+const btnShowInf = document.querySelector("#btnShowInfo");
 
 function showModal() {
   mdlChpt.classList.remove("hidden");
@@ -24,3 +25,28 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+function showInfUl(e) {
+  const eTar = e.target;
+  const infoText = document.querySelector(".info_ul");
+  if (eTar.classList.contains("fa-angle-double-down")) {
+    infoText.innerHTML =
+      "<ul> <li>Title: Alice's Adventures in Wonderland</li><li>Author: Lewis Carroll</li> <li>Release Date: June 25, 2008 [EBook #11]</li> <li>Last Updated: July 14, 2014</li> <li>Language: English</li> </ul>";
+    btnShowInf.classList.remove("fa-angle-double-down");
+    btnShowInf.classList.add("fa-angle-double-up");
+  } else if (eTar.classList.contains("fa-angle-double-up")) {
+    infoText.innerText = "";
+    btnShowInf.classList.remove("fa-angle-double-up");
+    btnShowInf.classList.add("fa-angle-double-down");
+  }
+}
+
+btnShowInf.addEventListener("click", showInfUl);
+
+/* <ul>
+<li>Title: Alice's Adventures in Wonderland</li>
+<li>Author: Lewis Carroll</li>
+<li>Release Date: June 25, 2008 [EBook #11]</li>
+<li>Last Updated: July 14, 2014</li>
+<li>Language: English</li>
+</ul> */
