36300a4226478c7e71b9f9379c74731c50d44cc9
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-28 19:07:11 -0300

    C2 Ex 2 & 3 B7D3

--- Fundamentos/bloco-7-introducao-JS-ES6-e-testesUnitarios/dia-3-Primeiros-passos-Jest/ex1/sum.test.js
+++ Fundamentos/bloco-7-introducao-JS-ES6-e-testesUnitarios/dia-3-Primeiros-passos-Jest/ex1/sum.test.js
@@ -3,3 +3,13 @@ const sum = require("./sum");
 test("sum two values", () => {
   expect(sum(4, 5)).toBe(9);
 });
+
+test("sum 0 0", () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test("text if f return error", () => {
+  expect(() => {
+    sum(4, "5");
+  }).toThrow();
+});
