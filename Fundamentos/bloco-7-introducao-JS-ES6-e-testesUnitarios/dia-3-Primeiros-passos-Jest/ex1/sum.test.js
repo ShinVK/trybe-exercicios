4597bcbd6843f35e1562dda86609c171c31f6758
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-29 00:03:40 -0300

    C3 Ex 3 B7D3

--- Fundamentos/bloco-7-introducao-JS-ES6-e-testesUnitarios/dia-3-Primeiros-passos-Jest/ex1/sum.test.js
+++ Fundamentos/bloco-7-introducao-JS-ES6-e-testesUnitarios/dia-3-Primeiros-passos-Jest/ex1/sum.test.js
@@ -13,3 +13,26 @@ test("text if f return error", () => {
     sum(4, "5");
   }).toThrow();
 });
+
+test("text if f return error message", () => {
+  expect(() => {
+    sum(4, "5");
+  }).toThrowError(new Error("parameters must be numbers"));
+});
+
+const myRemove = require("./myRemove.js");
+it("Teste o retorno", () => {
+  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+});
+
+const fizzBuzz = require("./fizzbuzz.js");
+
+it("Retorno esperado fizz, buzz, fizzbuzz ou nan", () => {
+  expect(fizzBuzz(15)).toBe("fizzbuzz");
+  expect(fizzBuzz(3)).toBe("fizz");
+  expect(fizzBuzz(5)).toBe("buzz");
+  expect(fizzBuzz(2)).toBe(2);
+  expect(fizzBuzz("a")).toBe(false);
+});
