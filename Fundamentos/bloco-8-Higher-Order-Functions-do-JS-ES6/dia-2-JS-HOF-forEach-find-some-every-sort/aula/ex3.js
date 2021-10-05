c4a59d66e8e8a902aaedfef698ea70e2513fa47b
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-05 16:22:57 -0300

    C6 Ex6 B8D2

--- Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-2-JS-HOF-forEach-find-some-every-sort/aula/ex3.js
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-2-JS-HOF-forEach-find-some-every-sort/aula/ex3.js
@@ -8,3 +8,20 @@ const hasName = (arr, name) => {
 };
 
 console.log(hasName(names, 'Ana'));
+
+// 2.Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 26 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((person) => person.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
