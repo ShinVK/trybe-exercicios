3e054431d5ac094e1423fca8e91c5da696b4d812
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-06 14:36:19 -0300

    Comments about last exercise from B8D2

--- Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-2-JS-HOF-forEach-find-some-every-sort/exercicios/ex7.js
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-2-JS-HOF-forEach-find-some-every-sort/exercicios/ex7.js
@@ -79,4 +79,18 @@ function authorUnique() {
   );
 }
 
+// o primeiro every vai verificar de acordo com o parâmetro passado
+// O parâmetro passado para o primeiro every é o some book.some, como funciona um book every - testa se todos os elementos do array passam pelo teste implementado pela função fornecida., Na verdade o primeiro parâmetro passado é o book, o argumento é o book.some que é uma outra função
+// books.every((par1) => argumento(book.some) )
+// Para ver se o parâmetro passa pelo argumento ele precisa iterar várias vezes até receber o resultado final - true ou false, true se passar de todos argumentos e false se falhar em algum.
+// Para passar em todos argumentos ele precisa que toda iteração do argumento bookSome de true para ser false.
+// Esquece tudo que escrevi acima;
+//
+//BASICAMENTE É PARA SER VERDADEIRO A CONDIÇÃO DO EVERY, NINGUÉM PODE TER NASCIDO NO MESMO ANO. Por isso utiliza o every. Ou seja se tiver alguém que nasceu no mesmo ano precisa retornar false. Mas a condição inicial dessa função retorna verdadeiro se todos requisitos forem cumpridos. E qual o requisito? é que não tenha ninguem que nasceu no mesmo ano.
+// portanto se tiver alguém que nasceu no mesmo ano retorna false.
+// se não tiver ninguem que tenha nascido no mesmo ano - retorna true.
+// a função escrita ta dizendo:
+//
+//
+
 assert.strictEqual(authorUnique(), expectedResult);
---
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-2-JS-HOF-forEach-find-some-every-sort/exercicios/teste.js
@@ -0,0 +1,11 @@
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+  teste();
+  return age;
+}
+
+function teste() {
+  console.log(`Eu tenho ${age}`);
+}
+
+calcAge(1994);
