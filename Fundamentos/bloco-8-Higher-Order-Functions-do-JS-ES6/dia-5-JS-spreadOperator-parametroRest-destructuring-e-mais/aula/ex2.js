65cffec0ea863b8ae63b3c632e0873bf3acd67a1
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-08 15:57:03 -0300

    C1.5 Ex5 Before Class B8D5

--- Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-5-JS-spreadOperator-parametroRest-destructuring-e-mais/aula/ex2.js
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-5-JS-spreadOperator-parametroRest-destructuring-e-mais/aula/ex2.js
@@ -32,3 +32,22 @@ console.log(numerosPares); // [6, 8, 10, 12];
 
 [, , , ...numerosPares] = numerosPares;
 console.log(numerosPares); // [6, 8, 10, 12];
+
+// Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined . Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
+
+const getNationality = ({ firstName, nationality = 'Brazilian' }) =>
+  `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
