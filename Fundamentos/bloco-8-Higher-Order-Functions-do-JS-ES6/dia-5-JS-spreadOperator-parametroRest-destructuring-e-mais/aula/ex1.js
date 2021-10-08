e057ff4b2d0c48e5a4655b4176dc2507b62d7629
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-08 15:29:47 -0300

    C1.1 Exercise 1 before Class B8D5

---
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-5-JS-spreadOperator-parametroRest-destructuring-e-mais/aula/ex1.js
@@ -0,0 +1,14 @@
+// Para fixar e praticar, vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens , faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread .
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Maça', 'Banana', 'Morango'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Leite condensado', 'Leite Ninho', 'Creme de Leite'];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+  return [...fruit, ...additional];
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
---
+++ Fundamentos/bloco-8-Higher-Order-Functions-do-JS-ES6/dia-5-JS-spreadOperator-parametroRest-destructuring-e-mais/aula/exemplos/exemplos.js
@@ -0,0 +1,51 @@
+const numbers = [1, 2, 3];
+
+const newArray = [...numbers, 4, 5, 6];
+console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(numbers); // [ 1, 2, 3 ]
+
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months); /* [
+  'JAN', 'FEV', 'MAR',
+  'ABR', 'MAI', 'JUN',
+  'JUL', 'AGO', 'SET',
+  'OUT', 'NOV', 'DEZ'
+] */
+
+/* Outro uso interessante do spread é no argumento de uma função que recebe vários parâmetros. No próximo exemplo, temos uma função que calcula o IMC (índice de massa corporal) de um paciente. A função recebe como parâmetros o peso e a altura da pessoa, e retorna o resultado arredondado do IMC. Podemos salvar os dados do paciente em um array e utilizar o spread para expandir esses valores no argumento da função que calcula o IMC:
+ */
+
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.7];
+
+console.log(imc(...patientInfo)); // 20.76
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+console.log(Math.max(...randomNumbers)); // 800
+console.log(Math.min(...randomNumbers)); // 5
+
+const people = {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+};
+
+const about = {
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer',
+};
+
+const customer = { ...people, ...about };
+console.log(customer); /* {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer'
+} */
