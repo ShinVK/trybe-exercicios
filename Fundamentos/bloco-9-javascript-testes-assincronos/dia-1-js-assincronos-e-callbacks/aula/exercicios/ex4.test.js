4cec33c5493f6146b4919652066d3f9b606e70a2
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-15 16:13:59 -0300

    C1.4 Example for assynchronous test in jest

---
+++ Fundamentos/bloco-9-javascript-testes-assincronos/dia-1-js-assincronos-e-callbacks/aula/exercicios/ex4.test.js
@@ -0,0 +1,39 @@
+// test('Não deveria passar!', () => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!');
+//   }, 500);
+// });
+
+// test('Não deveria passar!', (done) => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!');
+//     done();
+//   }, 500);
+// });
+
+// test('Não deveria passar!', (done) => {
+//   setTimeout(() => {
+//     try {
+//       expect(10).toBe(5);
+//       console.log('Deveria falhar!');
+//       done();
+//     } catch (error) {
+//       done();
+//     }
+//   }, 500);
+// });
+
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done(error); // Alteramos esta linha
+    }
+  }, 500);
+});
+
