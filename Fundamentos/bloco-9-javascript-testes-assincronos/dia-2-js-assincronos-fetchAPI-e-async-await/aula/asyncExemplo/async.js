b37a3957010c3adaa8120440dcea42c847bb35b0
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-18 15:47:46 -0300

    C1 Add examples from Class B9D2

---
+++ Fundamentos/bloco-9-javascript-testes-assincronos/dia-2-js-assincronos-fetchAPI-e-async-await/aula/asyncExemplo/async.js
@@ -0,0 +1,33 @@
+// Primeira forma de utilizar await
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+  return result
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+// segunda forma de utilizar o await
+
+const fetchJoke2 = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke();
+fetchJoke2();
