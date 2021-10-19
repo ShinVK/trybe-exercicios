48b5005b31e7c14a237564f21ae14b6bb3d93588
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-19 12:07:12 -0300

    Update Readme B9D3

--- Fundamentos/bloco-9-javascript-testes-assincronos/dia-3-jest-testes-assincronos/aula/teardown/ex2.test.js
+++ Fundamentos/bloco-9-javascript-testes-assincronos/dia-3-jest-testes-assincronos/aula/teardown/ex2.test.js
@@ -28,25 +28,25 @@ const removeCity = (city) => {
 //   expect(cities).toContain('Recife');
 // });
 
-beforeEach(() => {
-  cities = ['Pindamonhangaba'];
-});
+// beforeEach(() => {
+//   cities = ['Pindamonhangaba'];
+// });
 
-afterEach(() => {
-  cities = [];
-});
+// afterEach(() => {
+//   cities = [];
+// });
 
-test('Testa a função addCity utilizando o beforeEach', () => {
-  expect.assertions(3);
-  addCity('Piraporinha');
-  expect(cities).toHaveLength(2);
-  expect(cities).toContain('Pindamonhangaba');
-  expect(cities).toContain('Piraporinha');
-});
+// test('Testa a função addCity utilizando o beforeEach', () => {
+//   expect.assertions(3);
+//   addCity('Piraporinha');
+//   expect(cities).toHaveLength(2);
+//   expect(cities).toContain('Pindamonhangaba');
+//   expect(cities).toContain('Piraporinha');
+// });
 
-test('Testa a função removeCity utilizando o beforeEach', () => {
-  expect.assertions(2);
-  removeCity('Pindamonhangaba');
-  expect(cities).not.toContain('Pindamonhangaba');
-  expect(cities).toHaveLength(0);
-});
+// test('Testa a função removeCity utilizando o beforeEach', () => {
+//   expect.assertions(2);
+//   removeCity('Pindamonhangaba');
+//   expect(cities).not.toContain('Pindamonhangaba');
+//   expect(cities).toHaveLength(0);
+// });
