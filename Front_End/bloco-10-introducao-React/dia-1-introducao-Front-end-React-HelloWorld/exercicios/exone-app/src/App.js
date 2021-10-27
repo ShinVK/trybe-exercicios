d5cbbd5eb504a95161c745ca4e6250db93444e8b
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-27 18:52:57 -0300

    C2.2 Exercise 2 -After Lecture B10.1 -  Use array methods with react

--- Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/exone-app/src/App.js
+++ Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/exone-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return <li>{value}</li>;
+};
+
+const tasks = [
+  'Escovar os dentes',
+  'Arrumar a cama',
+  'Alongar',
+  'Tomar café',
+  'caminhada',
+  'estudar',
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <> {tasks.map((task) => Task(task))}</>;
 }
 
 export default App;
