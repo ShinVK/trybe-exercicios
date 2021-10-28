baf0384d5fab680afe9ff412931941e64ddfa9a7
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-28 00:12:24 -0300

    C2.3.3 Add Footer with H1 tag

--- Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/ex2/extwo-app/src/App.js
+++ Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/ex2/extwo-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import Content from './content';
+import Footer from './footer';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Content />
+        <Footer />
       </div>
     );
   }
