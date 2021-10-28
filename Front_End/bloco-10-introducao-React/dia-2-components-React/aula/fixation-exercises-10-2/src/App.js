5a1957845a6c4704a23bd81bc78d41a1486088d7
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-28 18:48:44 -0300

    C1.4 Last example before lecture

--- Front_End/bloco-10-introducao-React/dia-2-components-React/aula/fixation-exercises-10-2/src/App.js
+++ Front_End/bloco-10-introducao-React/dia-2-components-React/aula/fixation-exercises-10-2/src/App.js
@@ -1,17 +1,35 @@
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
-import Image from './image';
-// import urlImg from './cat-2083492_1280.webp';
-// const urlImg =
-//   'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
-function App() {
-  return (
-    <main>
-      <Image
-        source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'
-        alternativeText='Cute cat staring'
-      />
-    </main>
-  );
+import UserProfile from './UserProfile';
+
+class App extends React.Component {
+  render() {
+    const users = [
+      {
+        id: 102,
+        name: 'João',
+        email: 'joao@gmail.com',
+        avatar:
+          'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png',
+      },
+      {
+        id: 77,
+        name: 'Amélia',
+        email: 'amelia@gmail.com',
+        avatar:
+          'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png',
+      },
+    ];
+
+    return (
+      <div className='App'>
+        {users.map((user) => (
+          <UserProfile key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
