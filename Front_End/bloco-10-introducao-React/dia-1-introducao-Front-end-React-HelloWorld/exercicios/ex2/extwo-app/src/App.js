d2f50a60be874d90c03eb8c52b589dbcee5769c2
Victor Shin <victor.kamiguchi@gmail.com>
2021-10-28 00:06:58 -0300

    C2.3.2 Add content, using Map with React

--- Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/ex2/extwo-app/src/App.js
+++ Front_End/bloco-10-introducao-React/dia-1-introducao-Front-end-React-HelloWorld/exercicios/ex2/extwo-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './header';
+import Content from './content';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <Content />
       </div>
     );
   }
