a2bcc746b45bc269147c364bba4f6ebc54ccf497
Victor Shin <victor.kamiguchi@gmail.com>
2021-11-03 18:38:43 -0300

    Finishing class B11D11, exercise gonna be forked in a other folder exercicios2, if forked in a folder linked with gitHub, could be a problem

--- Front_End/bloco-11-componentes-com-EstadoEventoseFormularios-com-React/dia-1-componentes-com-estado-e-eventos/aula/ex-1/src/App.js
+++ Front_End/bloco-11-componentes-com-EstadoEventoseFormularios-com-React/dia-1-componentes-com-estado-e-eventos/aula/ex-1/src/App.js
@@ -46,11 +46,21 @@ class App extends React.Component {
     }))
   }
 
+  btnColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
+    const { numeroBt1, numeroBt2, numeroBt3 } = this.state;
     return ( <>
-      <button onClick={this.clique1}> {this.state.numeroBt1}</button>
-      <button onClick={this.clique2}>{this.state.numeroBt2} </button>
-      <button onClick={this.clique3}> {this.state.numeroBt3} </button>
+      <button onClick={this.clique1}
+      style={{backgroundColor: this.btnColor(numeroBt1)}} > {numeroBt1}</button>
+      <button onClick={this.clique2} 
+      style={{backgroundColor: this.btnColor(numeroBt2)}}
+      >{numeroBt2} </button>
+      <button onClick={this.clique3}
+      style={{backgroundColor: this.btnColor(numeroBt3)}}
+      > {numeroBt3} </button>
       </>
     )
     
