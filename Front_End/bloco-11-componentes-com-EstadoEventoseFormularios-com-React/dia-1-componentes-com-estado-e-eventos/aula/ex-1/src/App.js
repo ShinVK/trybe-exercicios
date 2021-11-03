c32b875d58b1169f39119ee997d10efdbcd92044
Victor Shin <victor.kamiguchi@gmail.com>
2021-11-03 15:28:12 -0300

    C1.2 Exercise 2 from B11D1 before lecture

--- Front_End/bloco-11-componentes-com-EstadoEventoseFormularios-com-React/dia-1-componentes-com-estado-e-eventos/aula/ex-1/src/App.js
+++ Front_End/bloco-11-componentes-com-EstadoEventoseFormularios-com-React/dia-1-componentes-com-estado-e-eventos/aula/ex-1/src/App.js
@@ -14,11 +14,33 @@ function clique3() {
 }
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.clique1 = this.clique1.bind(this)
+    this.clique2 = this.clique2.bind(this)
+    this.clique3 = this.clique3.bind(this)
+
+
+  }
+
+  clique1() {
+    console.log(this);
+    console.log('botao1');
+  }
+  
+  clique2() {
+    console.log('botao2');
+  }
+  
+  clique3() {
+    console.log('botao3');
+  }
+
   render() {
     return ( <>
-      <button onClick={clique1}> botao 1 </button>
-      <button onClick={clique2}> botao 2 </button>
-      <button onClick={clique3}> botao 3 </button>
+      <button onClick={this.clique1}> botao 1 </button>
+      <button onClick={this.clique2}> botao 2 </button>
+      <button onClick={this.clique3}> botao 3 </button>
       </>
     )
     
